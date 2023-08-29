65b3e9fa3eef98531f95c188fb6baad7e50f334b
linareswst <oscar.alejandro.linares@gmail.com>
2023-08-29 17:17:51 -0600

    Finalizando ejercicio agregando descripciones de producto

--- 02-Ejercicio/jaguar-sport/src/data.js
+++ 02-Ejercicio/jaguar-sport/src/data.js
@@ -3,7 +3,7 @@ export const data = [
         id: 1,
         name: "Colchoneta Everlast",
         category: "Yoga y Pilates",
-        description: "Producto x",
+        description: "Sumérgete en la serenidad y el equilibrio con nuestra colchoneta de yoga premium. Diseñada para proporcionarte una superficie cómoda y antideslizante, esta colchoneta te brinda el espacio perfecto para practicar tus poses y meditación.",
         price: 35,
         urlImage: "https://siman.vtexassets.com/arquivos/ids/883297-1200-auto?v=637382850682530000&width=1200&height=auto&aspect=true",
         quantity: 1
@@ -12,7 +12,7 @@ export const data = [
         id: 2,
         name: "Aro de pilates Everlast",
         category: "Yoga y Pilates",
-        description: "Producto x",
+        description: "Transforma tu rutina de ejercicios con nuestro aro de pilates de alta calidad. Diseñado para añadir resistencia y versatilidad a tu entrenamiento, este aro es una herramienta esencial para fortalecer y tonificar todo tu cuerpo.",
         price: 35,
         urlImage: "https://siman.vtexassets.com/arquivos/ids/806349-1200-auto?v=637340515919170000&width=1200&height=auto&aspect=true",
         quantity: 1
@@ -21,7 +21,7 @@ export const data = [
         id: 3,
         name: "Balón de futbol Nike",
         category: "Futbol",
-        description: "Producto x",
+        description: "Experimenta la emoción del fútbol en su máxima expresión con nuestro balón de fútbol de alta calidad. Diseñado para ofrecer un rendimiento excepcional en el campo, este balón combina durabilidad y precisión en cada patada.",
         price: 30,
         urlImage: "https://siman.vtexassets.com/arquivos/ids/4341724-1200-auto?v=638219214085130000&width=1200&height=auto&aspect=true",
         quantity: 1
@@ -30,7 +30,7 @@ export const data = [
         id: 4,
         name: "Camisa polo Umbro",
         category: "Futbol",
-        description: "Producto x",
+        description: "Eleva tu estilo casual con nuestra camisa polo de primera calidad. Con un diseño versátil y elegante, esta prenda combina comodidad y sofisticación en un solo paquete. El corte clásico y los detalles de calidad hacen que esta camisa sea perfecta.",
         price: 30,
         urlImage: "https://siman.vtexassets.com/arquivos/ids/4049402-1200-auto?v=638145821607400000&width=1200&height=auto&aspect=true",
         quantity: 1
@@ -39,7 +39,7 @@ export const data = [
         id: 5,
         name: "Maletín deportivo Adidas",
         category: "Mochilas deportivas",
-        description: "Producto x",
+        description: "Descubre la combinación perfecta de versatilidad y estilo con nuestra mochila deportiva de alto rendimiento. Diseñada para adaptarse a tu estilo de vida activo, esta mochila es tu aliada en todas tus aventuras.",
         price: 50,
         urlImage: "https://siman.vtexassets.com/arquivos/ids/4157610-1200-auto?v=638174254462430000&width=1200&height=auto&aspect=true",
         quantity: 1
@@ -48,7 +48,7 @@ export const data = [
         id: 6,
         name: "Maletín deportivo Under",
         category: "Mochilas deportivas",
-        description: "Producto x",
+        description: "Prepárate para llevar tu pasión deportiva a cualquier parte con nuestro maletín deportivo de primera calidad. Diseñado pensando en la comodidad y la funcionalidad, este maletín es el compañero perfecto para tus actividades atléticas y aventuras al aire libre.",
         price: 50,
         urlImage: "https://siman.vtexassets.com/arquivos/ids/2944245-1200-auto?v=637895291839030000&width=1200&height=auto&aspect=true",
         quantity: 1
@@ -57,7 +57,7 @@ export const data = [
         id: 7,
         name: "Bicicleta D'route",
         category: "Ciclismo",
-        description: "Producto x",
+        description: "Descubre la emoción de la velocidad y la libertad en nuestras bicicletas de alta rendimiento. Diseñadas para satisfacer tanto a ciclistas casuales como a entusiastas experimentados, nuestras bicicletas combinan tecnología avanzada con aerodinamismo.",
         price: 25,
         urlImage: "https://siman.vtexassets.com/arquivos/ids/4148801-1200-auto?v=638170860326330000&width=1200&height=auto&aspect=true",
         quantity: 1
@@ -66,7 +66,7 @@ export const data = [
         id: 8,
         name: "Scooter eléctrica",
         category: "Ciclismo",
-        description: "Producto x",
+        description: "Experimenta la libertad y la comodidad en movimiento con nuestro scooter eléctrico de vanguardia. Diseñado para brindarte una forma eficiente y sostenible de desplazarte, este scooter combina tecnología avanzada con un diseño elegante.",
         price: 25,
         urlImage: "https://siman.vtexassets.com/arquivos/ids/4411068-1200-auto?v=638237403895900000&width=1200&height=auto&aspect=true",
         quantity: 1
@@ -75,7 +75,7 @@ export const data = [
         id: 9,
         name: "Guantes Venum Elite",
         category: "Boxeo",
-        description: "Producto x",
+        description: "Descubre la combinación perfecta de estilo y funcionalidad con nuestros elegantes guantes. Diseñados para mantenerte cómodo y abrigado en cualquier ocasión, estos guantes son ideales para enfrentar los días fríos con confianza. ",
         price: 20,
         urlImage: "https://m.media-amazon.com/images/I/81HusU4ahqL._AC_SX425_.jpg",
         quantity: 1
@@ -84,7 +84,7 @@ export const data = [
         id: 10,
         name: "Saco boxeo Everlast",
         category: "Boxeo",
-        description: "Producto x",
+        description: "Desata tu energía y pasión por el boxeo con nuestro saco de boxeo de alta resistencia. Diseñado para soportar tus golpes más intensos, este saco es la herramienta esencial para entrenar, liberar estrés y mejorar tu forma física.",
         price: 20,
         urlImage: "https://siman.vtexassets.com/arquivos/ids/4417841-1200-auto?v=638240222441300000&width=1200&height=auto&aspect=true",
         quantity: 1
