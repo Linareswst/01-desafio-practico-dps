09e6e905faa4d19d32a16790dad84fb1237858f1
linareswst <oscar.alejandro.linares@gmail.com>
2023-08-29 17:03:36 -0600

    Agregando filtros por categoría

--- 02-Ejercicio/jaguar-sport/src/components/Headers.jsx
+++ 02-Ejercicio/jaguar-sport/src/components/Headers.jsx
@@ -1,120 +1,122 @@
 import React, { useState } from "react";
 
 export const Headers = ({
-    allProducts,
-    setAllProducts,
-    total,
-    countProducts,
-    setCountProducts,
-    setTotal,
-  }) => {
-    const [active, setActive] = useState(false);
+  allProducts,
+  setAllProducts,
+  total,
+  countProducts,
+  setCountProducts,
+  setTotal,
+}) => {
+  const [active, setActive] = useState(false);
+
+  const onDeleteProduct = (product) => {
+    const results = allProducts.filter((item) => item.id !== product.id);
+    setTotal(total - product.price * product.quantity);
+    setCountProducts(countProducts - product.quantity);
+    setAllProducts(results);
+  };
+
+  const onCleanCart = () => {
+    setAllProducts([]);
+    setTotal(0);
+    setCountProducts(0);
+  };
+
+  const updateProductQuantity = (product, newQuantity) => {
+    const updatedProducts = allProducts.map((item) => {
+      if (item.id === product.id) {
+        const updatedProduct = { ...item, quantity: newQuantity };
+        return updatedProduct;
+      }
+      return item;
+    });
+
+    setAllProducts(updatedProducts);
+    recalculateTotals(updatedProducts);
+  };
+
+  const recalculateTotals = (products) => {
+    const newTotal = products.reduce(
+      (acc, product) => acc + product.price * product.quantity,
+      0
+    );
+    const newCountProducts = products.reduce(
+      (acc, product) => acc + product.quantity,
+      0
+    );
+
+    setTotal(newTotal);
+    setCountProducts(newCountProducts);
+  };
+
   
-    const onDeleteProduct = (product) => {
-      const results = allProducts.filter((item) => item.id !== product.id);
-      setTotal(total - product.price * product.quantity);
-      setCountProducts(countProducts - product.quantity);
-      setAllProducts(results);
-    };
-  
-    const onCleanCart = () => {
-      setAllProducts([]);
-      setTotal(0);
-      setCountProducts(0);
-    };
-  
-    const updateProductQuantity = (product, newQuantity) => {
-      const updatedProducts = allProducts.map((item) => {
-        if (item.id === product.id) {
-          const updatedProduct = { ...item, quantity: newQuantity };
-          return updatedProduct;
-        }
-        return item;
-      });
-  
-      setAllProducts(updatedProducts);
-      recalculateTotals(updatedProducts);
-    };
-  
-    const recalculateTotals = (products) => {
-      const newTotal = products.reduce(
-        (acc, product) => acc + product.price * product.quantity,
-        0
-      );
-      const newCountProducts = products.reduce(
-        (acc, product) => acc + product.quantity,
-        0
-      );
-  
-      setTotal(newTotal);
-      setCountProducts(newCountProducts);
-    };
-  
-    return (
-      <header>
-        <h1>Jaguar Sport</h1>
-        <div className="container-icon">
-          <div className="container-cart-icon" onClick={() => setActive(!active)}>
-            <img
-              src="https://i.ibb.co/gd8RMcR/pngwing-com.png"
-              alt="carrito"
-              className="icon-cart"
-            />
-            <div className="count-products">
-              <span id="contador-productos">{countProducts}</span>
-            </div>
-          </div>
-          <div
-            className={`container-cart-products ${active ? "" : "hidden-cart"}`}
-          >
-            {allProducts.length ? (
-              <>
-                <div className="row-product">
-                  {allProducts.map((product) => (
-                    <div className="cart-product" key={product.id}>
-                      <div className="info-cart-product">
-                        <span className="cantidad-producto-carrito">
-                          <input
-                            className="cantidad-producto-carrito-input"
-                            type="number"
-                            value={product.quantity}
-                            onChange={(e) =>
-                              updateProductQuantity(product, e.target.value)
-                            }
-                          />
-                        </span>
-                        <span className="imagen-producto-carrito">
-                          <img src={product.urlImage} alt="imagen-producto" />
-                        </span>
-                        <p className="titulo-producto-carrito">{product.name}</p>
-                        <span className="precio-producto-carrito">
-                          ${product.price}
-                        </span>
-                      </div>
-                      <img
-                        src="https://i.ibb.co/FsMqpvW/trash-can.png"
-                        alt="cerrar"
-                        className="icon-close"
-                        onClick={() => onDeleteProduct(product)}
-                      />
-                    </div>
-                  ))}
-                </div>
-                <div className="cart-total">
-                  <h3>Total:</h3>
-                  <span className="total-pagar">${total}</span>
-                </div>
-                <span className="boton-vaciar-cart">
-                  <button className="btn-clear-all" onClick={onCleanCart}>
-                    Vaciar Carrito
-                  </button>
-                </span>
-              </>
-            ) : (
-              <p className="cart-empty">El carrito está vacío</p>
-            )}
+
+  return (
+    <header>
+      <h1>Jaguar Sport</h1>
+      <div className="container-icon">
+        <div className="container-cart-icon" onClick={() => setActive(!active)}>
+          <img
+            src="https://i.ibb.co/gd8RMcR/pngwing-com.png"
+            alt="carrito"
+            className="icon-cart"
+          />
+          <div className="count-products">
+            <span id="contador-productos">{countProducts}</span>
           </div>
         </div>
-      </header>
-    );
+        <div
+          className={`container-cart-products ${active ? "" : "hidden-cart"}`}
+        >
+          {allProducts.length ? (
+            <>
+              <div className="row-product">
+                {allProducts.map((product) => (
+                  <div className="cart-product" key={product.id}>
+                    <div className="info-cart-product">
+                      <span className="cantidad-producto-carrito">
+                        <input
+                          className="cantidad-producto-carrito-input"
+                          type="number"
+                          value={product.quantity}
+                          onChange={(e) =>
+                            updateProductQuantity(product, e.target.value)
+                          }
+                        />
+                      </span>
+                      <span className="imagen-producto-carrito">
+                        <img src={product.urlImage} alt="imagen-producto" />
+                      </span>
+                      <p className="titulo-producto-carrito">{product.name}</p>
+                      <span className="precio-producto-carrito">
+                        ${product.price}
+                      </span>
+                    </div>
+                    <img
+                      src="https://i.ibb.co/FsMqpvW/trash-can.png"
+                      alt="cerrar"
+                      className="icon-close"
+                      onClick={() => onDeleteProduct(product)}
+                    />
+                  </div>
+                ))}
+              </div>
+              <div className="cart-total">
+                <h3>Total:</h3>
+                <span className="total-pagar">${total}</span>
+              </div>
+              <span className="boton-vaciar-cart">
+                <button className="btn-clear-all" onClick={onCleanCart}>
+                  Vaciar Carrito
+                </button>
+              </span>
+            </>
+          ) : (
+            <p className="cart-empty">El carrito está vacío</p>
+          )}
+        </div>
+      </div>
+    </header>
+  );
 };
--- 02-Ejercicio/jaguar-sport/src/components/ProductList.jsx
+++ 02-Ejercicio/jaguar-sport/src/components/ProductList.jsx
@@ -1,45 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import { data } from "../data";
 
 export const ProductList = ({
-    allProducts,
-    setAllProducts,
-    total,
-    countProducts,
-    setCountProducts,
-    setTotal,
-  }) =>{
-      const onAddProduct = product => {
-          if(allProducts.find(item => item.id === product.id)){
-              const products = allProducts.map(item =>
-                  item.id === product.id
-                  ? {...item, quantity: item.quantity + 1}
-                  : item
-              );
-              setTotal(total + product.price * product.quantity);
-              setCountProducts(countProducts + product.quantity);
-              return setAllProducts([...products])
-          }
-          setTotal(total + product.price * product.quantity);
-          setCountProducts(countProducts + product.quantity);
-          setAllProducts([...allProducts, product]);
-      }
-      return (
-          <div className="container-items">
-              {data.map(product =>(
-                  <div className="item" key={product.id}>
-                      <figure>
-                          <img src={product.urlImage} alt={product.name} />
-                      </figure>
-                      <div className="info-product">
-                          <h1>{product.name}</h1>
-                          <p>{product.description}</p>
-                          <h3>Categoría: {product.category}</h3>
-                          <p className="price">${product.price}</p>
-                          <button className="btn-add-cart" onClick={() => onAddProduct(product)}>Añadir al carrito</button>
-                      </div>
-                  </div>
-              ))}
-          </div>
+  allProducts,
+  setAllProducts,
+  total,
+  countProducts,
+  setCountProducts,
+  setTotal,
+}) => {
+{/* Random products */}
+const shuffledProducts = [...data].sort(() => Math.random() - 0.5);
+
+{/* Filter by category */}
+  const [selectedCategory, setSelectedCategory] = useState("all");
+
+  const uniqueCategories = [
+    "all",
+    ...new Set(data.map((product) => product.category)),
+  ];
+
+  const filterProducts =
+    selectedCategory === "all"
+      ? shuffledProducts
+      : shuffledProducts.filter((product) => product.category === selectedCategory);
+
+
+  const onAddProduct = (product) => {
+    if (allProducts.find((item) => item.id === product.id)) {
+      const products = allProducts.map((item) =>
+        item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
       );
+      setTotal(total + product.price * product.quantity);
+      setCountProducts(countProducts + product.quantity);
+      return setAllProducts([...products]);
+    }
+    setTotal(total + product.price * product.quantity);
+    setCountProducts(countProducts + product.quantity);
+    setAllProducts([...allProducts, product]);
+  };
+  return (
+    <>
+      <div className="container-filters">
+        <h1>Filtrar por categorías: </h1>
+        <select className="select-filtros"
+          value={selectedCategory}
+          onChange={(e) => setSelectedCategory(e.target.value)}
+        >
+          {uniqueCategories.map((category) => (
+            <option value={category} key={category}>
+              {category === "all" ? "Todas las categorías" : category}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="container-items">
+        {filterProducts.map((product) => (
+          <div className="item" key={product.id}>
+            <figure>
+              <img src={product.urlImage} alt={product.name} />
+            </figure>
+            <div className="info-product">
+              <h1>{product.name}</h1>
+              <p>{product.description}</p>
+              <h3>Categoría: {product.category}</h3>
+              <p className="price">${product.price}</p>
+              <button
+                className="btn-add-cart"
+                onClick={() => onAddProduct(product)}
+              >
+                Añadir al carrito
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
